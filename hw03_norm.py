7f2b618b8a6af9b5e7b4322cdc38532289e19256
haema5 <killy.ny@gmail.com>
2018-10-03 17:37:12 +0300

    Задача 3 решена

--- hw03_norm.py
+++ hw03_norm.py
@@ -39,11 +39,33 @@ def sort_to_max(origin_list):
 print('Исходный список:', [2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 print('Отсортированный список:', sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
-
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+print()
+print('Задание-3:')
 
+
+def func(x):
+    if x > 0:
+        return True
+    else:
+        return False
+
+
+def filter_2(func, arg):
+    fil_lst = []
+    for i in arg:
+        if func(i) == False:
+            fil_lst.append(i)
+    return fil_lst
+
+
+lst = [2, 10, -12, 2.5, 20, -11, 4, 4, 0]
+lst = filter(func, lst)
+lst = list(lst)
+
+print(lst)
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
